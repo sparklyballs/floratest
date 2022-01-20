a8b3cd6727a1a2dbdbba04761d98598649256ed7
sparklyballs <sparklyballs@gmail.com>
2022-01-20 07:40:06 +0000

    use latest chia style with separate build and service env

--- docker-start.sh
+++ docker-start.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC2154
-if [[ ${farmer} == 'true' ]]; then
-  flora start farmer-only
-elif [[ ${harvester} == 'true' ]]; then
-  if [[ -z ${farmer_address} || -z ${farmer_port} || -z ${ca} ]]; then
-    echo "A farmer peer address, port, and ca path are required."
-    exit
-  else
-    flora configure --set-farmer-peer "${farmer_address}:${farmer_port}"
-    flora start harvester
-  fi
-else
-  flora start farmer
+flora start "${service}"
+
+trap "echo Shutting down ...; flora stop all -d; exit 0" SIGINT SIGTERM
+
+# shellcheck disable=SC2154
+if [[ ${log_to_file} == 'true' ]]; then
+  # Ensures the log file actually exists, so we can tail successfully
+  touch "$FLORA_ROOT/log/debug.log"
+  tail -F "$FLORA_ROOT/log/debug.log" &
 fi
 
-# Ensures the log file actually exists, so we can tail successfully
-touch "$CONFIG_ROOT/log/debug.log"
-tail -f "$CONFIG_ROOT/log/debug.log"
+while true; do sleep 1; done
